ba483a57757ecad236a16a08ed9235c99abc1339
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-10-05 22:20:47 +0100

    Project Setup done

---
+++ src/router/Router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter, type RouteObject } from "react-router";
+// import Navbar from "../components/Navbar";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import  Home from "../pages/Home";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      { path: "/login", element: <Login></Login> },
+      { path: "register", element: <Register></Register> },
+    ],
+  },
+];
+export const router = createBrowserRouter(routes);
