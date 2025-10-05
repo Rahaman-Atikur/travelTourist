783ed5b103ef710d936abcef922de4ed835d1f31
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-10-05 15:01:06 +0100

    First Commit -all setup done including react router

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h2 className="text-7xl font-extrabold">This is okay</h2>
+      <div className="navbar bg-base-100 shadow-sm">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+    </>
+  );
+}
+export default App;
