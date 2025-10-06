4daebf22a06f9b13e3f0718d05eed797901533bc
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-10-06 23:51:00 +0100

    Navbar Added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,29 @@
-
 import type React from "react";
-import bgImage from "../assets/images/Rectangle 1.png"
-import navIcon from "../assets/images/logo.png"
+import bgImage from "../assets/images/Rectangle 1.png";
+import navIcon from "../assets/images/logo.png";
+import { NavLink } from "react-router";
 
 const Navbar: React.FC = () => {
   return (
     <>
-   
-    <div >
-      <img src={bgImage} alt="" />
-    </div>
-    // Navbar 
-    <div>
-      <img src={navIcon} alt="" />
-
-    </div>
-
+      <div>
+        <img src={bgImage} alt="" />
+      </div>
+      {/* Navbar */}
+      <div className="flex justify-around content-center">
+        <img className="w-15 h-15" src={navIcon} alt="" />
+        <input 
+          type="text"
+          placeholder="neutral"
+          className="input input-neutral"
+        />
+        <NavLink to="/news">News</NavLink>
+        <NavLink to="/destination">Destination</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
+        <NavLink to="contact">Contact</NavLink>
+        <button className="btn primary">Login</button>
+      </div>
     </>
-
-    
   );
 };
 
