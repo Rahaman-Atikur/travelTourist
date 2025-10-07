27234d04052207673b67470e2f1baf2e4b841d3f
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-10-07 19:29:25 +0100

    Login Button is created

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,16 +2,20 @@ import { createBrowserRouter, type RouteObject } from "react-router";
 // import Navbar from "../components/Navbar";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import  Home from "../pages/Home";
+import Home from "../pages/Home";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <Home></Home>,
-    children: [
-      { path: "/login", element: <Login></Login> },
-      { path: "/register", element: <Register></Register> },
-    ],
+    // children: [
+    //   { path: "/login", element: <Login></Login> },
+    //   { path: "/register", element: <Register></Register> },
+    // ],
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
   },
 ];
 export const router = createBrowserRouter(routes);
